88f7e7e1f5bef72d5d8a04e57694d807401d80d6
Tarzan123 <lilmor12@nith.no>
2015-03-10 14:05:48 +0100

    Oppdatert scrip fil

--- Test666/Controllers/VideoPlayerController.cs
+++ Test666/Controllers/VideoPlayerController.cs
@@ -12,8 +12,7 @@ namespace Test666.Controllers
         // GET: api/VideoPlayer
         public IEnumerable<video> Get()
         {
-
-          /* VideoLinkDataContext db = new VideoLinkDataContext();
+            /* VideoLinkDataContext db = new VideoLinkDataContext();
 
            return db.videos.ToArray();*/
 
