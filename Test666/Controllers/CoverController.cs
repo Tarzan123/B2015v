e27919ca3f53c1fc5861c9da84404b0c081dd07d
Tarzan123 <lilmor12@nith.no>
2015-03-11 10:47:28 +0100

    Lagt til CoverController og initializeCover

---
+++ Test666/Controllers/CoverController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Test666.Controllers
+{
+    public class CoverController : ApiController
+    {
+        // GET: api/Cover
+        public IEnumerable<CoverDB> Get()
+        {
+            CoverLinkDataContext db = new CoverLinkDataContext();
+            
+            var data = from item in db.CoverDBs
+                       orderby item.Id
+                       select item;
+               return data; 
+        }
+
+        // GET: api/Cover/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Cover
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Cover/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Cover/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Test666/js/ajaxCover/initializeCover.js
@@ -0,0 +1,14 @@
+﻿
+//test
+    $.get("api/Cover", function (results) {
+        $.each(results, function (i, results) {
+
+            var x = results;
+            var id = x.Id;
+            var Navn = x.Navn;
+            var url = x.url;
+
+            $(".slides").append("<li> <img class='img-rounded' src='bilder/" + url + "'/> </li>");
+        });
+    });
+
